9da3fd089ef3f9d1459b6e374bb25f0bd68f62bc
МахамбетовИ <@local.itstep>
2017-05-23 19:50:13 +0600

    5 task done

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -15,6 +15,7 @@ namespace DZ_2
             {
                 Console.Clear();
                 Console.WriteLine("Меню:");
+                Console.WriteLine("0. Выход.");
                 Console.WriteLine("1. Вывод на экран в одну строку трех любых чисел с двумя пробелами между ними.");
                 Console.WriteLine("2. Вывести на экран числа 5, 10 и 21 одно под другим");
                 Console.WriteLine("3. Дано расстояние в сантиметрах. Найти число полных метров в нем");
@@ -63,6 +64,47 @@ namespace DZ_2
                         int days = 234;                       
                         Console.WriteLine("Количество дней: " + days);
                         Console.WriteLine("Количество полных недель: " + days/7);
+                        break;
+                    case 5:
+                        int num = 0;
+                        bool res;
+                        do
+                        {
+                            Console.Write("Введите двузначное число: ");
+                            res = Int32.TryParse(Console.ReadLine(), out num);
+                            
+                        }
+                        while (!res || num < 10 || num > 99);
+                        Console.WriteLine("a. Число десятков: " + num/10);
+                        Console.WriteLine("b. Число единиц: " + (num - (num / 10)*10));
+                        Console.WriteLine("c. Сумма цифр: " + ((num - (num / 10) * 10) + (num / 10)));
+                        Console.WriteLine("d. Произведение цифр: " + (num - (num / 10) * 10) * (num / 10));
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    case 14:
+                        break;
+                    case 15:
+                        break;
+                    case 16:
+                        break;
+                    case 17:
+                        break;
+                    case 18:
                         break;
                 }
 
